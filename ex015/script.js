247b288b79fdc8b6235a268531028f9c0611b125
Anna Clara Faria Gomes <annaclara1813@gmail.com>
2024-07-09 14:40:13 -0300

    exercicios js - condições

---
+++ ex015/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero =  ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src','nenemhomem.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovemHomem.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adultoHomem.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosoHomem.png')
+            }
+              
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'nenemMulher.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovemMulher.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adultaMulher.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosaMulher.png')
+            }
+
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
